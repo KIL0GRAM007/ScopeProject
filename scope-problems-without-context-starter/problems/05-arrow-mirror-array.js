823920fabfe7f058599b0b2c22c08e76b3515ab6
Kris Wilson <kristopherwilson193@gmail.com>
2022-05-19 18:22:20 -0500

    05 error

--- scope-problems-without-context-starter/problems/05-arrow-mirror-array.js
+++ scope-problems-without-context-starter/problems/05-arrow-mirror-array.js
@@ -10,9 +10,20 @@ arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
 arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
 const arrowMirrorArray = array => {
-      
-}
+     let newArr = []
+     for (let i = 0; i < array.length; i++){
+          let res1 = array[i]
+          newArr.push(res1)
+     }
+     for (let i = 0; i < array.length; i++){
+          let res2 = array[i]
+          let rev = array.reverse(res2)
+          newArr.push(rev)
+     }// return newArr
+     console.log(newArr)
 
+}
+arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
